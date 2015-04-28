8ec6bc0aa9dce64b82ab1a95714ca979bf83ce06
Angus Ferguson <a.ferguson@student.unimelb.edu.au>
2015-04-28 23:42:39 +0000

    Added ArchiveReference Field

--- minutes/documents/admin.py
+++ minutes/documents/admin.py
@@ -5,6 +5,9 @@ from documents.models import Person, Meeting, Document
 class PersonAdmin(admin.ModelAdmin):
 	list_display = ['__unicode__', 'last_name', 'title']
 
+class ArchiveReference(admin.ModelAdmin)
+	list_display = ['__unicode__', 'filenumber', 'series']
+
 class MeetingAdmin(admin.ModelAdmin):
 	list_display = ['__unicode__', 'title', 'date']
 
@@ -12,6 +15,7 @@ class DocumentAdmin(admin.ModelAdmin):
 	list_display = ['__unicode__', 'review_flag', 'doc_file']
 
 admin.site.register(Person, PersonAdmin)
+admin.site.register(ArhiveReference, ArchiveReferenceAdmin)
 admin.site.register(Meeting, MeetingAdmin)
 admin.site.register(Document, DocumentAdmin)
 
--- minutes/documents/models.py
+++ minutes/documents/models.py
@@ -1,52 +1,69 @@
 from django.db import models
 from django.utils.text import slugify
 
+
 class Person(models.Model):
-	title = models.CharField(max_length=50)
-	first_name = models.CharField(max_length=25)
-	last_name = models.CharField(max_length=25)
-	slug = models.SlugField(max_length=100)
+        title = models.CharField(max_length=50)
+        first_name = models.CharField(max_length=25)
+        last_name = models.CharField(max_length=25)
+        slug = models.SlugField(max_length=100)
 
-	def __unicode__(self):
-		return "%s %s" %(self.title, self.name())
+        def __unicode__(self):
+                return "%s %s" %(self.name(), self.title)
 
-	def name(self):
-		return "%s %s" %(self.first_name, self.last_name)
+        def name(self):
+                return "%s %s" %(self.last_name, self.first_name)
 
-	def return_last_name(self):
-		return self.last_name
+        def return_last_name(self):
+                return self.last_name
 
-	def save(self):
-		self.slug = slugify(self)
-		super(Person, self).save()
+        def save(self):
+                self.slug = slugify(self)
+                super(Person, self).save()
+
+
+class ArchiveReference(models.Model):
+        series = models.CharField(max_length = 50)
+        filenumber = models.CharField(max_length = 15)
+        dateclosed = models.DateField(blank=True, null=True)
+
+        def __unicode__(self):
+                return self.filenumber
+        
+        class Meta:
+                pass
+		#ordering = (unique id number for archive reference)
 
 
 class Meeting(models.Model):
-	title = models.CharField(max_length=100)
-	date = models.DateField()
-	members = models.ManyToManyField(Person, blank=True, null=True)
-	slug = models.SlugField(max_length=110)
+        title = models.CharField(max_length=100)
+        date = models.DateField()
+        members = models.ManyToManyField(Person, blank=True, null=True)
+        slug = models.SlugField(max_length=110)
+        filenumber = models.ForeignKey(ArchiveReference, blank=True, null=True)
 
-	def __unicode__(self):
-		return "%s %s" %(self.title, self.date)
-	
-	def save(self):
-		self.slug = slugify(self)
-		super(Meeting, self).save()
+        def __unicode__(self):
+                return "%s %s" %(self.title, self.date)
 
+        def save(self):
+                self.slug = slugify(self)
+                super(Meeting, self).save()
 
 class Document(models.Model):
-	title = models.CharField(max_length=150)
-	author = models.ManyToManyField(Person, blank=True, null=True)
-	review_flag = models.BooleanField(default=False)
-	doc_file = models.FileField()	
-	meeting = models.ManyToManyField(Meeting, blank=True, null=True)
-	slug = models.SlugField(max_length=150)
+        title = models.CharField(max_length=150)
+        author = models.ManyToManyField(Person, blank=True, null=True)
+        review_flag = models.BooleanField(default=False)
+        doc_file = models.FileField(blank=True, null=True)      
+        meeting = models.ManyToManyField(Meeting, blank=True, null=True)
+        archivereferences = models.ManyToManyField(ArchiveReference)
 
-	def __unicode__(self):
-		#authors_list = self.authors 
-		return self.title
+        slug = models.SlugField(max_length=150)
 
-	def save(self):
-		self.slug = slugify(self)
-		super(Document, self).save()	
+        def __unicode__(self):
+                #authors_list = self.authors 
+                return self.title
+
+        def save(self):
+                self.slug = slugify(self)
+                super(Document, self).save()    
+
